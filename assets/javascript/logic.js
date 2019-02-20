c0c8c25c03facc039b30628a68b1e9b092be172c
EF <ef@EFs-MacBook-Pro.local>
2019-02-20 10:30:48 -0500

    Random number chooser works, and displays name of the chosen child

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,26 +2,31 @@ console.log("js file is coming through");
 
 // variable to display who's turn it is
 let displayTurn = document.getElementById("turn");
-
+let chosen = "";
 // On-Click call the function
 document.addEventListener("click", event => {
+  // if the click event isn't on the "start" element, then do nothing
   if (!event.target.matches("#start")) return;
   event.preventDefault();
-  console.log(event.target);
+  // otherwise call the function
   choose();
 });
+
+// Random number chooser
 const choose = () => {
-  // variable for Harrison (min)
-  let harrison = 0;
-  // variable for Reese (max)
-  let reese = 10;
-// Randomly choose between max and min
-Math.random()
-// Set timeout of 4 sec
-// Display animation for 4 sec
-// Hide animation
+  // Randomly choose between 1 and 10 both inclusive
+  ranNum = Math.random() * (10 - 1 + 1) + 1;
+  console.log(ranNum);
+
+  // if number is less than or equal to 1, then it's Harrison's turn
+  if (ranNum <= 5) {
+    chosen = "Harrison";
+  } else chosen = "Reese";
+  displayTurn.innerHTML = chosen;
+  // Set timeout of 4 sec
+  // Display animation for 4 sec
+  // Hide animation
 };
-// if number is closer to Harrison then whosTrun is "Harrison"
 // Display "Harrison"
 // add animation
 // else if number is closer to Reese then whosTurn is "Reese"
