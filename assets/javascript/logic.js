738a7f71cb95ad1e8dd52a55418cd5b24f35c71b
EF <ef@EFs-MacBook-Pro.local>
2019-02-20 12:02:52 -0500

    added reset button and tada sound when chosen is displayed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,34 +1,41 @@
-console.log("js file is coming through");
-
 // variable to display who's turn it is
 let displayTurn = document.getElementById("turn");
+// Variable to hold chosen person
 let chosen = "";
-// On-Click call the function
+let tada = new Audio("assets/sound/Ta Da-SoundBible.com-1884170640.mp3");
+
+// On-Click call choose function
 document.addEventListener("click", event => {
   // if the click event isn't on the "start" element, then do nothing
   if (!event.target.matches("#start")) return;
   event.preventDefault();
-  // otherwise call the function
-  choose();
+  // otherwise call the function after 4 seconds
+  setTimeout(choose, 3000);
 });
 
 // Random number chooser
 const choose = () => {
-  // Randomly choose between 1 and 10 both inclusive
+  // Randomly choose between 1 and 10 both inclusive as defined in above variable
   ranNum = Math.random() * (10 - 1 + 1) + 1;
   console.log(ranNum);
-
-  // if number is less than or equal to 1, then it's Harrison's turn
+  // if ranNum is less than or equal to 5, then it's Harrison's turn
   if (ranNum <= 5) {
     chosen = "Harrison";
-  } else chosen = "Reese";
+  } else chosen = "Reese"; // otherwise, it's Reese's turn
   displayTurn.innerHTML = chosen;
-  // Set timeout of 4 sec
+
+  // Tada sound plays when the person is chosen
+  tada.play();
   // Display animation for 4 sec
   // Hide animation
 };
-// Display "Harrison"
-// add animation
-// else if number is closer to Reese then whosTurn is "Reese"
-// Display "Reese"
-// add animation
+
+// Reset with a reset button
+document.addEventListener("click", event => {
+  // if the click event isn't on the "reset" element, then do nothing
+  if (!event.target.matches("#reset")) return;
+  event.preventDefault();
+  // otherwise reset clear turn element
+  chosen = "";
+  displayTurn.innerHTML = chosen;
+});
